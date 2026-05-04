8a105d6dee71e4a6750659e19dc99e9c3e39103a
ashish191289 <testashqa2qgmail.com>
2026-05-03 21:24:33 -0700

    change 02

--- tests/my_first01.spec.js
+++ tests/my_first01.spec.js
@@ -6,11 +6,31 @@ const {test,expect} = require('@playwright/test')
 // import {hello} from './demo/hello'
 //console.log (hello());
 
-test('My first',async ({page}) => {
+test('My first01',async ({page}) => {
 
-await page.goto('https://google.com')
-await expect(page).toHaveTitle('Google')
-await expect(page).toHaveTitle('Google')
+await page.goto('https://shop.lululemon.com/')
+//await page.locator('.modal_closeButton__rGjHL').click()
+await page.getByTitle('Close').click();
+await page.locator('//a[text()="Women"]').hover()
+await page.getByTestId('l1-0-l3-item-Coats & Jackets').click();
+//await page.getByText('Coats & Jackets').click()
+//await page.pause()
+await page.locator('//a[contains(text(),"Always Effortless Jacket")]').click()
+//await page.getByTitle('Light Ivory').click()
+await page.getByRole('radio', { name: 'Light Ivory' }).click();
+await page.getByRole('radio', { name: 'Black' }).click();
+await page.getByRole('radio', { name: 'XXS' }).nth(-1).click();
+//await page.getByRole('button',{name:'Add to Bag'}).click()
+await page.getByRole('button', { name: 'Add to Bag', exact: true }).click();
+await page.getByRole('link', { name: 'View Bag & Checkout' }).click()
+//await page.locator('//span[contains(text(),"XXS")]').click()
+await page.pause()
+
+
+
+
+
+
 
 
 })
